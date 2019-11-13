64f0b3f440372a1e5c47c1d59e0f58ade2c3ca17
yohanmishkin <yohanmishkin@protonmail.com>
2019-11-13 15:33:24 -0500

    Began styling <PersonCard />
    - installed styled components

--- src/components/ui/PersonCard/index.js
+++ src/components/ui/PersonCard/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-export default function({ person }) {
+const StyledLink = styled(Link)`
+  display: block;
+  margin-bottom: 1rem;
+  text-align: center;
+`;
+
+const Name = styled.h2`
+  color: green;
+`;
+
+const Temperature = styled.h3`
+  color: green;
+`;
+
+const PersonCard = ({ person }) => {
   return (
-    <Link to={`/people/${person.id}`} className="card">
-      <h2>{person.name}</h2>
-      <h3>{person.temperature}</h3>
-    </Link>
+    <StyledLink to={`/people/${person.id}`}>
+      <Name>{person.name}</Name>
+      <Temperature>{person.temperature}</Temperature>
+    </StyledLink>
   );
-}
+};
+
+export default PersonCard;
