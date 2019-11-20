e069739453b908973f4af47062949e6bc1b74482
yohanmishkin <yohanmishkin@protonmail.com>
2019-11-20 09:31:28 -0500

    Styling

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -5,7 +5,7 @@ import React from 'react';
 export default function() {
   return (
     <>
-      <h1 className="font-playfair">Weather station</h1>
+      <h1 className="font-playfair">WeatherStation</h1>
 
       <PeopleContainer>
         {({ people }) => <PeopleGrid people={people} />}
--- src/components/ui/PersonProfile/index.js
+++ src/components/ui/PersonProfile/index.js
@@ -10,40 +10,73 @@ const StyledLink = styled(Link)`
 `;
 
 const Profile = styled.div`
-  align-items: center;
   display: flex;
   flex-direction: column;
+  flex-grow: 1;
 
   img {
     border-radius: 0.35rem;
+    margin: auto;
+    margin-bottom: 1rem;
+    margin-top: unset;
     max-width: 20rem;
     width: 100%;
   }
 
-  ol {
-    list-style: none;
-    padding-left: unset;
+  h1 {
+    flex-grow: 1;
+    margin: auto;
+    margin-bottom: 1rem;
+    margin-top: unset;
+  }
+
+  dl {
+    display: flex;
+    overflow-x: scroll;
+    margin-bottom: 4rem;
+    min-height: 6rem;
+    scroll-snap-type: x mandatory;
+    scroll-padding: 50%;
+  }
+
+  .row {
+    background-color: white;
+    border-radius: 0.5rem;
+    margin-left: 2rem;
+    min-width: 6rem;
+    padding: 1rem;
+    scroll-snap-align: center;
+  }
+
+  dt {
+    font-size: 1.125rem;
+    font-weight: bold;
+    text-align: center;
+    margin-bottom: 0.5rem;
+  }
+
+  dd {
+    margin-left: unset;
   }
 `;
 
 const PersonProfile = ({ person }) => {
   return (
-    <div>
+    <>
       <StyledLink to="/">Back to people</StyledLink>
       <Profile>
         <img alt={`${person.name}'s headshot`} src={person.imageUrl} />
         <h1 className="font-playfair">{person.name}</h1>
-        <h2>{person.temperature}</h2>
         <dl>
           {person.forecasts.map((forecast, index) => (
-            <div key={index}>
+            <div className="row" key={index}>
               <dt>{forecast.period}</dt>
               <dd>{forecast.shortDescription}</dd>
             </div>
           ))}
         </dl>
       </Profile>
-    </div>
+    </>
   );
 };
 
