055bdf12f334cfa06bb64bc2c0911fda28b027f6
yohanmishkin <yohanmishkin@protonmail.com>
2019-11-25 00:33:45 -0500

    Revert "Trying viewport height trick"
    This reverts commit 2771a14adfc10addfda8439c7b57982fd285ed86.

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,6 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 function App() {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-  window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
-
   return (
     <Switch>
       <Route exact path="/">
