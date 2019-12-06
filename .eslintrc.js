0f0e9d6e18f84c77f893888bb8dc4f389b8eef0b
yohanmishkin <yohanmishkin@protonmail.com>
2019-12-06 14:27:38 -0500

    Code split page components

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,12 +32,13 @@ module.exports = {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
   },
+    parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     },
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   settings: {
     react: {
--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,24 @@
-import HomePage from './pages/HomePage';
-import PersonPage from './pages/PersonPage';
 import SourceCodeLinks from './ui/SourceCodeLinks';
-import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+
+const HomePage = lazy(() => import('./pages/HomePage'));
+const PersonPage = lazy(() => import('./pages/PersonPage'));
 
 function App() {
   return (
     <SourceCodeLinks>
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
 
-        <Route path="/people/:id">
-          {({ match }) => <PersonPage id={match.params.id} />}
-        </Route>
-      </Switch>
+          <Route path="/people/:id">
+            {({ match }) => <PersonPage id={match.params.id} />}
+          </Route>
+        </Switch>
+      </Suspense>
     </SourceCodeLinks>
   );
 }
